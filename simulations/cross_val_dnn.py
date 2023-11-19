779e9afb7f53e829f83fd24b6aa9ee5150113972
mumcusena <mumcusena.222@gmail.com>
2023-11-19 16:35:37 +0300

    implemented early stopping

--- simulations/cross_val_dnn.py
+++ simulations/cross_val_dnn.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from keras.callbacks import EarlyStopping
 from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -27,13 +28,15 @@ n_splits = 10
 kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
 
 # Initialize arrays to store training and validation loss for each fold
-all_train_loss = []
-all_val_loss = []
-mse_scores = []
+all_train_loss = np.zeros((n_splits, 500))  # 500 is the maximum number of epochs
+all_val_loss = np.zeros((n_splits, 500))    # 500 is the maximum number of epochs
+mse_scores = []      # List to store MSE scores for each fold
 
+# Define early stopping criteria
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 
 # Perform cross-validation
-for train_index, test_index in kf.split(X_scaled):
+for i, (train_index, test_index) in enumerate(kf.split(X_scaled)):
     X_train, X_temp, y_train, y_temp = train_test_split(X_scaled, y, test_size=0.3, random_state=42)
     X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
 
@@ -50,14 +53,18 @@ for train_index, test_index in kf.split(X_scaled):
     # Compile the model
     model.compile(optimizer='adam', loss='mean_squared_error')
 
-    # Train the model
-    history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_val, y_val), verbose=0)
+    # Train the model with early stopping
+    history = model.fit(X_train, y_train, epochs=500, batch_size=32,
+                        validation_data=(X_val, y_val), callbacks=[early_stopping], verbose=0)
+    
+    # Evaluate the model on the test set
     mse = model.evaluate(X_test, y_test, verbose=0)
     mse_scores.append(mse)
+
     # Store training and validation loss for each fold
-    all_train_loss.append(history.history['loss'])
-    all_val_loss.append(history.history['val_loss'])
-    
+    all_train_loss[i, :len(history.history['loss'])] = history.history['loss']
+    all_val_loss[i, :len(history.history['val_loss'])] = history.history['val_loss']
+
 
 # Calculate the average MSE across all folds
 average_mse = np.mean(mse_scores)
@@ -69,13 +76,24 @@ print(f'Standard Deviation of Mean Squared Error: {mse_std_dev}')
 baseline_mse = np.mean((y - np.mean(y))**2)
 print(f'Baseline Mean Squared Error: {baseline_mse}')
 
+# Calculate the average training and validation loss across all folds
+avg_train_loss = np.mean(all_train_loss, axis=0)
+avg_val_loss = np.mean(all_val_loss, axis=0)
+
 # Plot average training and validation loss
 avg_train_loss = np.mean(all_train_loss, axis=0)
 avg_val_loss = np.mean(all_val_loss, axis=0)
+
 
 plt.plot(avg_train_loss, label='Average Training Loss')
 plt.plot(avg_val_loss, label='Average Validation Loss')
 plt.xlabel('Epochs')
 plt.ylabel('Mean Squared Error')
 plt.legend()
+plt.show()
+
+# Create a box plot for the MSE scores
+plt.boxplot(mse_scores)
+plt.title('Mean Squared Error across Folds')
+plt.ylabel('Mean Squared Error')
 plt.show()
